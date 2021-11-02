ff817b7f9c2be80daae4c7616ecb91cb31c1234a
yasuaki-344 <ikausay.443@gmail.com>
2021-11-02 21:57:12 +0900

    Implement example use case

---
+++ src/use_cases/example_interactor.py
@@ -0,0 +1,31 @@
+"""ユースケース実装の一例."""
+
+from business_logic.example_business_logic import ExampleBusinessLogic
+from entities.example_data_class import ExampleDataClass
+from repositories.example_repository import ExampleRepository
+
+
+class ExampleInteractor:
+    """ユースケースクラス実装の一例."""
+
+    def __init__(self,
+                 logic: ExampleBusinessLogic,
+                 repository: ExampleRepository):
+        """ユースケースコンストラクタの一例.
+
+        Args:
+            logic (ExampleBusinessLogic): ユースケースで使用するビジネスロジック
+            repository (ExampleRepository): ユースケースで使用するリポジトリ
+        """
+        self.__logic = logic
+        self.__repository = repository
+
+    def get_info(self) -> ExampleDataClass:
+        """ビジネスロジックとリポジトリを組み合わせて実現するユースケースの一例.
+
+        Returns:
+            ExampleDataClass: 返却するデータクラス
+        """
+        entity = self.__repository.get_entity()
+        entity.name = self.__logic.to_upper(entity.name)
+        return entity
