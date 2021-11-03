eba2d0e07361792a2bfb817b6fb603e9fbc132c7
yasuaki-344 <ikausay.443@gmail.com>
2021-11-03 10:55:41 +0900

    Add sample page view

--- src/app.py
+++ src/app.py
@@ -9,13 +9,13 @@ from views.plot_sample_view import PlotSampleView
 
 
 @inject
-def main(app1: ExampleView = Provide[Container.view1],
-         app2: PlotSampleView = Provide[Container.view2]):
+def main(example_view: ExampleView = Provide[Container.view1],
+         plot_sample_view: PlotSampleView = Provide[Container.view2]):
     """Set up and run application."""
     app = MultiApp()
     # ここでページの追加を行う.
-    app.add_app("1st page", app1.to_view)
-    app.add_app("2nd page", app2.to_view)
+    app.add_app("1st page", example_view.to_view)
+    app.add_app("plot sample page", plot_sample_view.to_view)
 
     # アプリケーションの実行
     app.run()
--- src/views/plot_sample_view.py
+++ src/views/plot_sample_view.py
@@ -1,5 +1,7 @@
 """複数ページ検証用に用意した簡易的なビュークラス."""
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 
 class PlotSampleView:
@@ -7,5 +9,16 @@ class PlotSampleView:
 
     def to_view(self):
         """実際に表示するページ要素の作成."""
-        st.title('another page')
-        st.write('Welcome to another page')
+        st.title('Plot sample page')
+        st.write('1次関数を描画するためサンプルページ')
+        st.number_input("傾き", -100, 100, 1)
+        st.number_input("切片", -100, 100, 0)
+        st.button("描画")
+        data = {
+            'x': np.random.random(20),
+            'y': np.random.random(20) - 0.5,
+            'z': np.random.random(20) - 1.0,
+        }
+        df = pd.DataFrame(data).set_index('x')
+        st.subheader('Line Chart')
+        st.line_chart(df)
